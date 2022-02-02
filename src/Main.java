be837185495319b2255a8439d304b1c760dfe683
iojuch <petrovich8384@mail.ru>
2022-02-02 19:22:09 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 500;
+        int passengerCount = 4;
+        int taxiPrice = 300;
+        int totaSum = ticketPrice * passengerCount + taxiPrice;
+        System.out.println(totaSum);
+
+    }
+}
